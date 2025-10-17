8f352f85e74f181e9b7d688e8b0f1347bc16bce6
Yashwant <42735365+yashwants1@users.noreply.github.com>
2025-10-17 23:16:34 +0530

    Update rg.tf

--- RG/rg.tf
+++ RG/rg.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.49.0"
     }
   }
-backend "azurerm" {
+
+  backend "azurerm" {
     resource_group_name  = "test-rg"
     storage_account_name = "yteststg4343"
     container_name       = "test"
@@ -15,11 +16,11 @@ backend "azurerm" {
 
 provider "azurerm" {
   features {}
-    
+
   subscription_id = "b3e9b690-1f69-4cae-899a-6a1c65b2db91"
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "y-rg"
+  name     = "y-rg"
   location = "East US"
 }
